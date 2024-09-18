991329f969346d0caffa3d78e26b35b483a954af
Tarcísio <tarcisiomenezes2019@hotmail.com>
2024-09-18 14:32:18 +0200

    Google gemini AI API collected and set up

---
+++ environmental_engineering_ai/src/lib/gemini.js
@@ -0,0 +1,17 @@
+import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from '@google-cloud/text-to-speech';
+
+const safetySetting = [
+    {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+    {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_LOW_AND_ABOVE,
+    },
+]
+const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GEMINI_AI_OUBLIC_KEY);
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", safetySetting});
+
+export default model;
