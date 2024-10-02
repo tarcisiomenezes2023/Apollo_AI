734395d50f19db9fa093bf54c66c65f1c4be94a0
Tarcísio <tarcisiomenezes2019@hotmail.com>
2024-10-02 16:19:22 +0200

    Layout Home page done.

--- apollo_ai/src/app/layout.tsx
+++ apollo_ai/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Link from "next/link";
 
 
 export const metadata: Metadata = {
@@ -14,11 +15,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <header>
-          HEADER
+      <body className="flex flex-col h-screen !p-4 !px-16">
+        <header className="flex items-center justify-between">
+          <Link href="/" className="flex items-center font-bold gap-8">
+            <img src="/logo3.png" alt="Logo" className="h-16 w-16" />
+            <span 
+            className="ml-1 text-4xl font-bold
+            transition duration-500 hover:bg-gradient-to-r hover:from-[#33ff28] 
+            hover:to-[#e55571] hover:bg-clip-text hover:text-transparent">
+              Homepage
+            </span>
+          </Link>
+          <div className="">Users</div>
         </header>
-        {children}
+        <main className="flex-1 overflow-hidden">
+          {children}
+        </main>
         <footer>
           FOOTER
         </footer>
